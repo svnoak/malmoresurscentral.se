442d3995c0f6d5c9637626ca30713dc123bb8687
Kimpfimp <kimpfimp@gmail.com>
2020-12-22 16:00:03 +0100

    Initial commit
    mail.php contains script for sending mail via html form

---
+++ mail.php
@@ -0,0 +1,30 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$dropdown = $_POST['service'];
+$message = $_POST['message'];
+$formcontent="Namn: $name \nKontaktmejl: $email \nTjänst: $dropdown \nMeddelande: $message";
+$recipient = "kontakt@malmoresurscentral.se";
+$subject = "Mejl ang: $dropdown";
+$mailheader = "From: $email \r\n";
+if(!empty($_POST['website'])) die();
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+?>
+
+<head>
+    <link rel="stylesheet" href="./mvp.css">
+    <meta charset="utf-8">
+    <meta name="description" content="Malmö Resurscentral">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resurscentralen</title>
+</head>
+<body>
+<section>
+	<aside>
+		<h3>Ditt meddelande har skickats &#10003;</h3>
+		<a href="index.html"><b>Gå tillbaka</b></a>
+	</aside>
+</section>
+</body>
+<footer>
+</footer>
